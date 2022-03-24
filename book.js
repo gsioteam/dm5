6f7053c921bec08a0813d7ad4bb11a3e1de6a697
Ftbom <lz490070@gmail.com>
2022-03-24 21:47:23 +0800

    test

--- book.js
+++ book.js
@@ -17,6 +17,7 @@ class BookController extends Controller {
             title: this.data.title,
             subtitle: this.data.subtitle,
             picture: this.data.picture,
+            summary: this.data.summary,
             page: 'book',
             data: {
                 link: this.url,
