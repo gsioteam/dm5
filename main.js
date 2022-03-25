6eb8ec930927ad395c8c9d700b7da2d252475dd4
Ftbom <lz490070@gmail.com>
2022-03-25 08:07:30 +0800

    fix bug again

--- main.js
+++ main.js
@@ -201,17 +201,15 @@ class MainController extends Controller {
                     picture: image_list[this.text_recommend.indexOf(text)]
                 });
                 for (let data of json[8]['data']) {
-                    if (data['type'] == 1) {
-                        results.push({
-                            title: data['title'],
-                            subtitle: data['authors'],
-                            picture: data['cover'],
-                            pictureHeaders: {
-                                Referer: url
-                            },
-                            link: `http://api.dmzj.com/dynamic/comicinfo/${data['id']}.json`,
-                        });
-                    }
+                    results.push({
+                        title: data['title'],
+                        subtitle: data['authors'],
+                        picture: data['cover'],
+                        pictureHeaders: {
+                            Referer: url
+                        },
+                        link: `http://api.dmzj.com/dynamic/comicinfo/${data['id']}.json`,
+                    });
                 }
             } else {
                 results.push({
@@ -220,17 +218,19 @@ class MainController extends Controller {
                     picture: image_list[this.text_recommend.indexOf(text)]
                 });
                 for (let data of json['data']['data']){
-                    let sub_title = data['sub_title'];
-                    sub_title = sub_title.substring(3,sub_title.length)
-                    results.push({
-                        title: data['title'],
-                        subtitle: sub_title,
-                        picture: data['cover'],
-                        pictureHeaders: {
-                            Referer: url
-                        },
-                        link: `http://api.dmzj.com/dynamic/comicinfo/${data['obj_id']}.json`,
-                    });
+                    if (data['type'] == 1) {
+                        let sub_title = data['sub_title'];
+                        sub_title = sub_title.substring(3,sub_title.length)
+                        results.push({
+                            title: data['title'],
+                            subtitle: sub_title,
+                            picture: data['cover'],
+                            pictureHeaders: {
+                                Referer: url
+                            },
+                            link: `http://api.dmzj.com/dynamic/comicinfo/${data['obj_id']}.json`,
+                        });
+                    }
                 }
             }
         }
